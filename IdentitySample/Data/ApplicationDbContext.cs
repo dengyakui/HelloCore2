6d5df876cd40e6402622416cef04640d832497fb
dengyakui <297354229@qq.com>
2018-09-21 18:49:16 +0800

    add enw mvc project

---
+++ IdentitySample/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+namespace IdentitySample.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>,Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+    
+}
---
+++ IdentitySample/Data/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace IdentitySample.Data
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string CustomTag { get; set; }
+    }
+    
+}
