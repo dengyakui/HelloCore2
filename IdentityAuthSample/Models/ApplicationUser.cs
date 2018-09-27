157afa52242226ee75c2926863d32a2716e54fee
dengyakui <297354229@qq.com>
2018-09-27 13:49:15 +0800

    add identity samle proj

---
+++ IdentityAuthSample/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityAuthSample.Models
+{
+    public class ApplicationUser:IdentityUser<Guid>
+    {
+        public string CustomTag { get; set; }
+    }
+}
